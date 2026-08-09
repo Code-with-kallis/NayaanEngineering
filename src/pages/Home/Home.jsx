9ffdbe97ef2fe7dce6acdbca5b138450d305309f
Ovaise Qayoom <qovaise@gmail.com>
2026-08-09 21:34:56 +0530

    Update home page and Junaid image

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -428,7 +428,7 @@ const Home = () => {
 
         <div className={styles.splitContentGrid}>
           <div className={styles.splitImageCard}>
-            <img src="/assets/projects/proj-03.webp" alt="Site Supervision" />
+            <img src="/assets/team/junaid.jpg" alt="Site Supervision" />
           </div>
 
           <div className={styles.featuresList}>
